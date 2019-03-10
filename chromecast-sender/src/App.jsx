3764ababb17499feed3e5eb4c2c274a50656f33d
simonarp <s_arp@gmx.de>
2019-03-10 12:28:32 +0100

    add virtual keyboard to chromecast sender; set background-color to white to display it correctly under chromecast

--- chromecast-sender/src/App.jsx
+++ chromecast-sender/src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import {resetGame} from './chromecast-messages';
+import {resetGame, keyboardPress} from './chromecast-messages';
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
+import keycode from 'keycode';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -10,14 +13,25 @@ export default class App extends React.Component {
           window.cast.framework.CastContext.getInstance().getCurrentSession()
               .sendMessage('urn:x-cast:mi136-hangman', resetGame())
       }
+      this.onKeyPress = (button) => {
+        if (window.cast.framework.CastContext.getInstance().getCurrentSession() !== null)
+          window.cast.framework.CastContext.getInstance().getCurrentSession()
+            .sendMessage('urn:x-cast:mi136-hangman', keyboardPress({ keyCode: keycode(button) }))
+      }
     }
 
     render() {
         return <div className="app">
-			<div className="castButton">
-                <google-cast-launcher></google-cast-launcher>
-            </div>
-			<button onClick={this.onResetClicked}>New Game</button>
-        </div>
+                <div className="castButton">
+                  <google-cast-launcher></google-cast-launcher>
+                </div>
+                <div className="resetButton">
+                  <button onClick={this.onResetClicked}>New Game</button>
+                </div>
+                 <Keyboard
+                  onKeyPress={button =>
+                    this.onKeyPress(button)}
+                />
+                </div>
     }
 }
