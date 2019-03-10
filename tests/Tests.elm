7ca01bd3d5ce6f3cb0e08df9d7c619ca7680ede4
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2019-03-10 19:57:50 +0100

    Added tests for the reducer

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -21,41 +21,114 @@ initialState = {
 all : Test
 all =
     describe "reducer"
-        [ describe "reset"
+        -- input char test uses the chromecastkeypress action as the RawKey of the 'normal' action is hard to create
+        --- both action call the same undelying code, so it should be fine
+        [ describe "inputchar"
             [
-                test "reset works" <|
+                test "solves letters if matching" <|
                     \_ ->
                         Expect.equal ({
-                            progress = [ Unsolved 'f', Unsolved 'o', Unsolved 'o'],
+                            progress = [ Unsolved 't', Solved 'e', Unsolved 's', Unsolved 't'],
+                            inputLetters = Set.fromList ['e'],
+                            won = InProgress,
+                            triesLeft = 13,
+                            words = [],
+                            usedWords = []
+                        }, (Cmd.none)) (update (ChromecastKeyPress (Char.toCode 'e')) {
+                            progress = [ Unsolved 't', Unsolved 'e', Unsolved 's', Unsolved 't'],
                             inputLetters = Set.fromList [],
                             won = InProgress,
                             triesLeft = 13,
                             words = [],
-                            usedWords = ["test"]
-                        }, (Cmd.none)) (update Reset {
-                            progress = [ Solved 't', Solved 'e', Solved 's', Solved 't'],
-                            inputLetters = Set.fromList [],
-                            won = Won,
-                            triesLeft = 8,
-                            words = ["foo"],
                             usedWords = []
                         }),
-                test "reset cycles used and normal words when normal words get emptied" <|
+                test "doesn't solve letters if not matching" <|
                     \_ ->
                         Expect.equal ({
-                            progress = [ Unsolved 'f', Unsolved 'o', Unsolved 'o'],
+                            progress = [ Unsolved 't', Unsolved 'e', Unsolved 's', Unsolved 't'],
+                            inputLetters = Set.fromList ['x'],
+                            won = InProgress,
+                            triesLeft = 12,
+                            words = [],
+                            usedWords = []
+                        }, (Cmd.none)) (update (ChromecastKeyPress (Char.toCode 'x')) {
+                            progress = [ Unsolved 't', Unsolved 'e', Unsolved 's', Unsolved 't'],
                             inputLetters = Set.fromList [],
                             won = InProgress,
                             triesLeft = 13,
-                            words = ["test"],
+                            words = [],
                             usedWords = []
-                        }, (Cmd.none)) (update Reset {
-                            progress = [ Solved 't', Solved 'e', Solved 's', Solved 't'],
+                        }),
+                test "solves multiple letters if multiple match" <|
+                    \_ ->
+                        Expect.equal ({
+                            progress = [ Solved 't', Unsolved 'e', Unsolved 's', Solved 't'],
+                            inputLetters = Set.fromList ['t'],
+                            won = InProgress,
+                            triesLeft = 13,
+                            words = [],
+                            usedWords = []
+                        }, (Cmd.none)) (update (ChromecastKeyPress (Char.toCode 't')) {
+                            progress = [ Unsolved 't', Unsolved 'e', Unsolved 's', Unsolved 't'],
                             inputLetters = Set.fromList [],
+                            won = InProgress,
+                            triesLeft = 13,
+                            words = [],
+                            usedWords = []
+                        }),
+                test "marks game as won if the last letter was solved" <|
+                    \_ ->
+                        Expect.equal ({
+                            progress = [ Solved 't', Solved 'e', Solved 's', Solved 't'],
+                            inputLetters = Set.fromList ['t', 'e', 's'],
                             won = Won,
                             triesLeft = 13,
                             words = [],
-                            usedWords = ["foo"]
+                            usedWords = []
+                        }, (Cmd.none)) (update (ChromecastKeyPress (Char.toCode 's')) {
+                            progress = [ Solved 't', Solved 'e', Unsolved 's', Solved 't'],
+                            inputLetters = Set.fromList ['t', 'e'],
+                            won = InProgress,
+                            triesLeft = 13,
+                            words = [],
+                            usedWords = []
                         })
-            ]
+            ],
+            describe "reset"
+                [
+                    test "reset works" <|
+                        \_ ->
+                            Expect.equal ({
+                                progress = [ Unsolved 'f', Unsolved 'o', Unsolved 'o'],
+                                inputLetters = Set.fromList [],
+                                won = InProgress,
+                                triesLeft = 13,
+                                words = [],
+                                usedWords = ["test"]
+                            }, (Cmd.none)) (update Reset {
+                                progress = [ Solved 't', Solved 'e', Solved 's', Solved 't'],
+                                inputLetters = Set.fromList [],
+                                won = Won,
+                                triesLeft = 8,
+                                words = ["foo"],
+                                usedWords = []
+                            }),
+                    test "reset cycles used and normal words when normal words get emptied" <|
+                        \_ ->
+                            Expect.equal ({
+                                progress = [ Unsolved 'f', Unsolved 'o', Unsolved 'o'],
+                                inputLetters = Set.fromList [],
+                                won = InProgress,
+                                triesLeft = 13,
+                                words = ["test"],
+                                usedWords = []
+                            }, (Cmd.none)) (update Reset {
+                                progress = [ Solved 't', Solved 'e', Solved 's', Solved 't'],
+                                inputLetters = Set.fromList [],
+                                won = Won,
+                                triesLeft = 13,
+                                words = [],
+                                usedWords = ["foo"]
+                            })
+                ]
         ]
